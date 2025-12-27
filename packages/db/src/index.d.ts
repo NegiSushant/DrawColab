038b6c8585af21efb39a3c0c0250b6dcad791522
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2025-12-27 06:36:21 +0530

    cleaning up structure

--- packages/db/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client.js";
-declare const prismaClient: PrismaClient;
-export { prismaClient };
-//# sourceMappingURL=index.d.ts.map
